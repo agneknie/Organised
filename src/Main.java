91120b55351772b0640161252e10504466af5c3a
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2021-01-08 20:08:07 +0200

    Start on the Login page

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Loads login page
+        Parent root = FXMLLoader.load(getClass().getResource("/views/LoginPageView.fxml"));
+
+        // Sets window (stage) to default size
+        primaryStage.setScene(new Scene(root, 1400, 900));
+
+        // Makes scene non resizable and removes toolbar
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        // Shows the window
+        primaryStage.show();
+    }
+}
