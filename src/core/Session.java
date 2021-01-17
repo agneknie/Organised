6e2e2060ab81e96d1b6604ef2b0e2f8d3f66854e
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2021-01-17 17:46:53 +0200

    Session additions for Marks functionality

--- src/core/Session.java
+++ src/core/Session.java
@@ -13,10 +13,14 @@ public class Session {
     private static User loggedUser = null;
     // Whether non-logged in user has just created a new user by registration
     private static boolean userCreatedInSession;
-    // Year selected in Marks tab
+    // Marks: which Year selected
     private static Year marksYearSelected = null;
-    // Module selected in Marks tab
+    // Marks: which Module selected
     private static Module marksModuleSelected = null;
+    // Marks: which Assignment selected
+    private static Assignment marksAssignmentSelected = null;
+    // Marks: whether add or edit button is clicked
+    private static String marksWhichButton = null;
 
     /**
      * Cleans the session by resetting the logged in user to null.
@@ -55,5 +59,72 @@ public class Session {
      */
     public static void setUserCreatedInSession(boolean userCreatedInSession) {
         Session.userCreatedInSession = userCreatedInSession;
+    }
+
+    /**
+     * Getter for currently selected Year in Marks tab.
+     * @return Year object
+     */
+    public static Year getMarksYearSelected() {
+        return marksYearSelected;
+    }
+
+    /**
+     * Setter for the Year which is currently selected by the user.
+     * @param marksYearSelected Year to set
+     */
+    public static void setMarksYearSelected(Year marksYearSelected) {
+        Session.marksYearSelected = marksYearSelected;
+    }
+
+    /**
+     * Getter for currently selected Module in Marks tab.
+     * @return Module object
+     */
+    public static Module getMarksModuleSelected() {
+        return marksModuleSelected;
+    }
+
+    /**
+     * Setter for the Module which is currently selected by the user.
+     * @param marksModuleSelected Module to set
+     */
+    public static void setMarksModuleSelected(Module marksModuleSelected) {
+        Session.marksModuleSelected = marksModuleSelected;
+    }
+
+    /**
+     * Getter for value of a button, which is clicked
+     * in Marks tab. Used for setting up the popup scene.
+     * @return Either "Edit" or "Add"
+     */
+    public static String getMarksWhichButton() {
+        return marksWhichButton;
+    }
+
+    /**
+     * Setter for the variable, which determines what button was clicked in Marks
+     * tab and is used for popup stage scene setup.
+     * Parameter can only be "Edit" or "Add".
+     * @param marksWhichButton "Edit" or "Add"
+     */
+    public static void setMarksWhichButton(String marksWhichButton) {
+        Session.marksWhichButton = marksWhichButton;
+    }
+
+    /**
+     * Getter for currently selected Assignment in Marks tab.
+     * @return Assignment object
+     */
+    public static Assignment getMarksAssignmentSelected() {
+        return marksAssignmentSelected;
+    }
+
+    /**
+     * Setter for the Assignment which is currently selected by the user.
+     * @param marksAssignmentSelected Assignment to set
+     */
+    public static void setMarksAssignmentSelected(Assignment marksAssignmentSelected) {
+        Session.marksAssignmentSelected = marksAssignmentSelected;
     }
 }
