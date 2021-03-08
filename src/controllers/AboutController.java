a11200edc2a873f696c1c7f28530529004f734d6
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2021-03-09 00:40:41 +0200

    About scene frontend

--- src/controllers/AboutController.java
+++ src/controllers/AboutController.java
@@ -1,6 +1,15 @@
 package controllers;
 
 import controllers.utilities.DefaultNavigation;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Organised.
@@ -12,6 +21,87 @@ import controllers.utilities.DefaultNavigation;
  *
  * Class which handles About Tab functionality and UI.
  */
-public class AboutController extends DefaultNavigation {
+public class AboutController extends DefaultNavigation implements Initializable {
+    // User Guide Button
+    @FXML
+    private Pane userGuideButton;
+    @FXML
+    private ImageView userGuideButtonImage;
+    @FXML
+    private Label userGuideButtonLabel;
 
+    // Behind the Scenes Button
+    @FXML
+    private Pane behindTheScenesButton;
+    @FXML
+    private ImageView behindTheScenesButtonImage;
+    @FXML
+    private Label behindTheScenesButtonLabel;
+
+    // Copyright & Resources Button
+    @FXML
+    private Pane copyrightResourcesButton;
+    @FXML
+    private ImageView copyrightResourcesButtonImage;
+    @FXML
+    private Label copyrightResourcesButtonLabel;
+
+    // Text areas for information display
+    @FXML
+    private TextArea userGuideText;
+    @FXML
+    private TextArea behindTheScenesText;
+    @FXML
+    private TextArea copyrightResourcesText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    // Methods handling button clicks
+    @FXML
+    private void behindTheScenesButtonClicked() {
+        //TODO behindTheScenesButtonClicked
+    }
+
+    @FXML
+    private void copyrightResourcesButtonClicked() {
+        //TODO copyrightResourcesButtonClicked
+    }
+
+    @FXML
+    private void userGuideButtonClicked() {
+        //TODO userGuideButtonClicked
+    }
+
+    // Methods styling the buttons
+    @FXML
+    private void behindTheScenesButtonExited() {
+
+    }
+
+    @FXML
+    private void behindTheScenesButtonHovered() {
+
+    }
+
+    @FXML
+    private void copyrightResourcesButtonExited() {
+
+    }
+
+    @FXML
+    private void copyrightResourcesButtonHovered() {
+
+    }
+
+    @FXML
+    private void userGuideButtonExited() {
+
+    }
+
+    @FXML
+    private void userGuideButtonHovered() {
+
+    }
 }
